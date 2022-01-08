abb5a74a3d0713e5b869fb99be04514fcd11f673
Mikhail Kononenko <5304864+vbifonixor@users.noreply.github.com>
2022-01-09 01:15:45 +0300

    Finish data transformation

--- scripts/generate-icons.js
+++ scripts/generate-icons.js
@@ -5,8 +5,51 @@ const {FIGMA_TOKEN} = process.env;
 
 const FIGMA_API_HOST = 'https://api.figma.com/v1';
 const ICONS_FILE_KEY = 'yYOcOaqdNOd40vLvc29Dws';
+const CONTAINER_NODE_TYPE = 'FRAME';
+const ICON_NODE_TYPE = 'COMPONENT';
+
+const Types = {
+  Mono: 'mono',
+  Multi: 'multi',
+  Illustration: 'illustration',
+  Stub: 'stub',
+  Unknown: 'unknown',
+};
+
+const PREFIXES = {
+  [Types.Mono]: 'mono-',
+  [Types.Multi]: 'multi-',
+  [Types.Illustration]: 'illustration-',
+  [Types.Stub]: 'stub-',
+};
+
+const getTypeByPrefix = (name) => {
+  return Object.entries(PREFIXES).reduce((acc, [type, prefix]) => {
+    if (name.startsWith(prefix)) {
+      return type;
+    }
+    return acc;
+  }, Types.Unknown);
+};
+
+const IGNORED_TYPES = [Types.Stub, Types.Unknown];
 
 const agent = superagent.agent();
+
+const transformFigmaNode = (
+  {name, id},
+  parentType = Types.Unknown,
+) => {
+  const iconExplicitType = getTypeByPrefix(name);
+  return {
+    name,
+    id,
+    type:
+      iconExplicitType !== Types.Unknown
+        ? iconExplicitType
+        : parentType,
+  };
+};
 
 async function main() {
   if (!FIGMA_TOKEN) {
@@ -30,15 +73,31 @@ async function main() {
     process.exit(1);
   }
 
-  console.log(
-    figmaDocument?.document?.children
-      .map(({children}) => children)
-      .reduce((acc, children) => [...acc, ...children])
-      .filter(({type}) => type === 'FRAME'),
-  );
+  const iconNodes = figmaDocument?.document?.children
+    .map(({children}) => children)
+    .reduce((acc, pageNodes) => [...acc, ...pageNodes], [])
+    .reduce((acc, node) => {
+      if (
+        node.type === CONTAINER_NODE_TYPE &&
+        node?.children?.length
+      ) {
+        const containerType = getTypeByPrefix(node.name);
+        return [
+          ...acc,
+          ...node.children
+            .map((childNode) =>
+              childNode.type === ICON_NODE_TYPE
+                ? transformFigmaNode(childNode, containerType)
+                : null,
+            )
+            .filter(Boolean),
+        ];
+      } else if (node.type === ICON_NODE_TYPE) {
+        return [...acc, transformFigmaNode(node)];
+      }
 
-  // .map((el) => el.children)
-  // .reduce((acc, children) => [...acc, ...children], [])
-  // .filter((child) => child.type === 'FRAME'),
+      return acc;
+    }, [])
+    .filter(({type}) => !IGNORED_TYPES.includes(type));
 }
 main();
