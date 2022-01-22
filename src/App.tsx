ad76c27dac6642911cfc77f7c7d9e8ca04f271fa
Mikhail Kononenko <5304864+vbifonixor@users.noreply.github.com>
2022-01-22 10:02:11 +0300

    Use some icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import science from './components/Icon/img/illustrations/science.svg';
+import sunsetAboveOcean from './components/Icon/img/multi/sunset-above-ocean.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={science} className="App-logo" alt="logo" />
+        <p><img src={sunsetAboveOcean} className="App-world"/> Hello, world!</p>
       </header>
     </div>
   );
